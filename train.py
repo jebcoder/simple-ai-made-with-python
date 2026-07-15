80d8ae366021542479c2d4131d3418c3c0c3e7e5
jeb <jeb774455@gmail.com>
2026-07-15 15:47:41 +0200

    Refactor training logic in train.py
    Removed loop and print statements; simplified training call.

--- train.py
+++ train.py
@@ -1,13 +1,6 @@
-import res.values as values
-import res.functions as functions
+import values
+import functions
 import random
 
-i = 0
-while i <= 100:
-    functions.train(values.numbers[random.randint(0, 9)])
-    i = i + 1
-    print(f"""
-You completed {i} questions,
-you must finish another {100 - i} questions.
-if you sabotage, I WILL KNOW.
-""")
+functions.train(values.numbers[random.randint(0, 9)])
+    
